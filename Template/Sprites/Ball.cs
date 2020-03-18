053ab9b3c0a167c9a28879fa12a8cb56e2223a03
Anders Huber <andershuber@mail.com>
2020-03-18 08:14:07 +0100

    Jag började med mitt projekt från början. Det första jag gjorde var att lägga till paddle, ball klass. Jag använde mig av objekt för att skapa två paddlar samt en boll som just nu är fixerad på skärmen. Paddlarna går att röra upp och ned samt har skärmens begränsningar. Jag började också med score systemet då jag körde fast när jag skulle göra att bollen rörde sig. Paddel klasserna har Update metod och LoadContent metod, det har också boll klassen så att det kan interagera med main klasen. Jag la också till interaktion mellan boll och vägg samt scoresystemet att varje gång bollen åker utanför skärmen blir poängen++. Men bollen rör sig inte än.
    Jag började med att skapa Texture2D och sedan paddelns position. Jag skapade en konstruktor och jag la in Texture2D:n som värde i den. Jag skapade en update och en loadcontent så att den kan röra på sig genom if satser. Jag skapade ett objekt av paddel klassen så att jag kunde använda paddel klassen i (main). Jag blev inte helt klar med paddel klassen, den saknar hitbox samt kollision vilket kommer bli mitt nästa mål.

---
+++ Template/Sprites/Ball.cs
@@ -0,0 +1,137 @@
+﻿
+
+using System;
+
+using Microsoft.Xna.Framework;
+
+using Microsoft.Xna.Framework.Input;
+
+using Microsoft.Xna.Framework.Graphics;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+namespace Pong
+
+{
+
+    class Paddle
+
+
+
+    {
+
+        private Texture2D paddle;
+        public Vector2 paddlePos;
+        private float fart = 5;
+        private Rectangle paddleHitbox;
+
+
+
+
+
+
+
+        public Paddle(Texture2D paddle)
+
+        {
+
+            this.paddle = paddle;
+
+
+
+        }
+
+
+        public void LoadContent(Paddle leftPaddle, Paddle rightPaddle)
+        {
+
+            leftPaddle.paddlePos.X = 0;
+            leftPaddle.paddlePos.Y = 340;
+
+            rightPaddle.paddlePos.X = 1179;
+            rightPaddle.paddlePos.Y = 340;
+
+
+        }
+
+        public Rectangle PaddleHitbox
+        {
+
+            get { return new Rectangle((int)paddlePos.X, (int)paddlePos.Y, 21, 148); }
+            set { paddleHitbox = value; }
+
+        }
+
+
+
+        public void Update(Paddle leftPaddle, Paddle rightPaddle, KeyboardState kstate)
+        {
+
+
+            if (kstate.IsKeyDown(Keys.Up))
+            {
+                rightPaddle.paddlePos.Y -= fart;
+            }
+
+            if (kstate.IsKeyDown(Keys.Down))
+            {
+                rightPaddle.paddlePos.Y += fart;
+            }
+
+            if (kstate.IsKeyDown(Keys.W))
+            {
+                leftPaddle.paddlePos.Y -= fart;
+            }
+
+            if (kstate.IsKeyDown(Keys.S))
+            {
+                leftPaddle.paddlePos.Y += fart;
+            }
+
+            if (paddlePos.Y <= 0)
+            {
+                paddlePos.Y = 0;
+            }
+
+            if (paddlePos.Y >= 1052)
+            {
+                paddlePos.Y = 340;
+            }
+
+            paddleHitbox.Location = paddlePos.ToPoint();
+
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+
+            spriteBatch.Draw(paddle, paddlePos, Color.White);
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
+
+
+
---
+++ Template/Sprites/Paddle.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+using Microsoft.Xna.Framework.Input;
+
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pong
+
+{
+    class Ball
+    {
+
+        private Texture2D ball;
+        private Vector2 ballPos;
+        private Rectangle ballHitbox;
+        public float velocity = 6;
+
+
+
+
+        public Ball(Texture2D ball)
+        {
+
+            this.ball = ball;
+
+        }
+
+        public Texture2D Boll
+        {
+
+            get { return ball; }
+            set { ball = value; }
+
+        }
+
+        public Rectangle BallHitbox
+        {
+
+            get { return new Rectangle((int)ballPos.X, (int)ballPos.Y, 19, 19); }
+            set { ballHitbox = value; }
+
+        }
+
+
+        public void LoadContent(Ball ball1)
+        {
+
+            ball1.ballPos.X = 340;
+            ball1.ballPos.Y = 600;
+
+        }
+
+
+        public void Update(Ball ball1, float velocity)
+        {
+
+            ball1.ballPos.X += velocity;
+            ball1.ballPos.Y += velocity;
+
+            if (ballPos.Y > 1179)
+            {
+                velocity *= -1;
+
+            }
+
+            if (ballPos.Y < 0)
+            {
+                velocity *= -1;
+            }
+
+            if (ballPos.X <= 0)
+            {
+
+
+            }
+
+            if (ballPos.X > 1200)
+            {
+
+
+            }
+
+
+            ballHitbox.Location = ballPos.ToPoint();
+
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(ball, ballPos, Color.White);
+
+        }
+
+
+
+    }
+}
